a515e7871610bad84386b55a0e20f4528a2c7ef4
Miohitokiri <lltzpp@gmail.com>
2018-04-05 18:29:21 +0800

    edited and solved UVa 10130

--- C++/UVa/Q10130.cpp
+++ C++/UVa/Q10130.cpp
@@ -1,30 +1,102 @@
-#include<iostream>
-#include<cstring>
-#include<cmath>
+// by. MiohitoKiri5474
+#include<bits/stdc++.h>
+
+#pragma GCC optimize ( "O3" )
+#pragma loop_opt ( on )
+
 using namespace std;
+
+typedef long long LL;
+
+// define tools
+#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
+#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
+#define REPALL(i,n) for ( auto &i: n )
+#define debuger cout << "111\n"
+#define MEM(n,i) memset ( n, i, sizeof n )
+
+// define pair
+typedef pair < LL, LL > pll;
+typedef pair < int, int > pii;
+#define F first
+#define S second
+#define mp make_pair
+
+// define vector && some stl's api
+template < class T > using vec = vector < T >;
+typedef vec < int > vi;
+typedef vec < LL > vl;
+#define pb push_back
+#define ep emplace_back
+#define REV reverse
+#define SZ(n) ( int ) n.size()
+#define CLR(n) n.clear()
+#define BEG(n) n.begin()
+#define END(n) n.end()
+#define EMP(n) n.empty()
+#define RSZ(n,s) n.resize ( s )
+#define ALL(n) BEG ( n ), END ( n )
+#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
+#define GETDATA(data,n) RSZ ( data, n ); REPALL ( i, data ) cin >> i
+
+// define set
+typedef set < int > si;
+typedef set < LL > sl;
+#define FID(n,Index) ( n.find ( Index ) != n.end() )
+
+// graph
+#define GRE(T,edge) vec < T > edge[maxN]
+#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
+#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
+
+// IO
+#define GL(n) getline ( cin, n )
+
+// define stack, queue, pri-queue
+template < class T > using stack = stack < T, vec < T > >;
+template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
+template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
+
+// define stringstream
+#define sstr stringstream
+
+// number~ remember change maxN
+#define INF 0x3f3f3f3f
+#define maxN 1005
+
+// ready~ go!
+// let's coding and have fun!
+// I can solve this problem!
+
+int weigh[maxN], value[maxN], dp[maxN][35];
+
 int main(){
-	long long t;
+	ios::sync_with_stdio ( false );
+	cin.tie ( 0 );
+	cout.tie ( 0 );
+
+	int n, t, w, ans, m;
 	cin >> t;
-	for ( long long times = 0 ; times < t ; t++ ){
-		long long N, W, ans = 0;
-		cin >> N >> W;
-		long long w[N], v[N];
-		for ( long long i = 0 ; i < N ; i++ )
-			cin >> v[i] >> w[i];
-		long long P;
-		cin >> P;
-		for ( long long i = 0 ; i < P ; i++ ){
-			long long p, ma;
-			cin >> p;
-			long long dp[N][p];
-			memset ( dp, 0, sizeof ( dp ) );
-			for ( long long i = 0 ; i < N ; i++ )
-				for ( long long j = W - w[i] ; j >= 0 ; j-- ){
-					dp[i][j] = max ( dp[i - 1][j], dp[i - 1][j - w[i]] + v[i] );
-					ma = max ( ma, dp[i][j] );
-				}
-			ans += ma;
+	while ( t-- ){
+		cin >> n;
+		for ( int i = 1 ; i <= n ; i++ )
+			cin >> value[i] >> weigh[i];
+		cin >> m;
+		MEM ( dp, 0 );
+		ans = 0;
+		for ( int i = 1 ; i <= n ; i++ ){
+			for ( int j = 0 ; j <= 30 ; j++ ){
+				if ( j - weigh[i] < 0 )
+					dp[i][j] = dp[i - 1][j];
+				else
+					dp[i][j] = max ( dp[i - 1][j], dp[i - 1][j - weigh[i]] + value[i] );
+			}
 		}
-		cout << ans << endl;
+		while ( m-- ){
+			cin >> w;
+			ans += dp[n][w];
+		}
+
+		cout << ans << '\n';
 	}
 }
