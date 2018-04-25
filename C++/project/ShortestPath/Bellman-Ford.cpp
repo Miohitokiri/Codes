1e4845ea57180ba7b75f4bddc040808a70607be7
Miohitokiri <lltzpp@gmail.com>
2018-04-25 23:19:07 +0800

    Bellman-Ford

---
+++ C++/project/ShortestPath/Bellman-Ford.cpp
@@ -0,0 +1,27 @@
+// Bellman-Ford
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define maxN 100005
+#define pb push_back
+typedef pair < int, int > pii;
+
+vector < pii > edges[maxN];
+int dis[maxN];
+
+inline void Bellman-Ford ( int start ){
+	memset ( dis, 0x3f3f3f, sizeof dis );
+	queue < int > q;
+	q.push ( start );
+	while ( !q.empty() ){
+		int now = q.front();
+		q.pop();
+		for ( auto i: edges[now] ){
+			if ( dis[i.first] > dis[now] + i.second ){ // 檢查是否能更新
+				q.push ( i.first );
+				dis[i.first] = dis[now] + i.second;
+			}
+		}
+	}
+}
