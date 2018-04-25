e63cb654d0cd5c929f8913ccd448a41f633db239
Miohitokiri <lltzpp@gmail.com>
2018-04-25 14:53:42 +0800

    kruskal ( MST )

--- C++/project/MST/Kruskal.cpp
+++ C++/project/MST/Kruskal.cpp
@@ -1,72 +1,46 @@
-// by. MiohitoKiri5474
 #include<bits/stdc++.h>
-
-#pragma GCC optimize ( "O3" )
-#pragma loop_opt ( on )
 
 using namespace std;
 
-typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPALL(i,n) for ( auto &i: n )
-
-#define mp make_pair
-
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
+#define maxN 10005
 #define pb push_back
-#define ep emplace_back
-#define SZ(n) ( int ) n.size()
-#define ALL(n) BEG ( n ), END ( n )
+typedef pair < int, int > pii;
 
-#define GRE(T,edge) vec < T > edge[maxN]
-
-#define maxN 100005
-
-// disjion Set
-struct disjoinSet{
-	int dis[maxN];
-
-	inline void Init ( void ){
-		REPP ( i, 0, maxN ) dis[i] = i;
-	}
-
-	inline int find ( int n ){
-		return dis[n] == n ? n : dis[n] = find ( dis[n] );
-	}
-
-	inline bool same ( int a, int b ){
-		return find ( a ) == find ( b );
-	}
-
-	inline void Union ( int a, int b ){
-		dis[find ( a )] = find ( b );
-	}
-};
-
-// edge's data
-struct bridge{
+struct node{
 	int u, v, w;
 };
 
-vec < bridge > edge;
-GRE ( pii, mst );
-
-inline bool cmp ( bridge a, bridge b ){
+inline bool cmp ( node a, node b ){
 	return a.w < b.w;
 }
 
+int dis[maxN];
+vector < node > edges;
+vector < pii > mst[maxN];
+
+inline void init ( void ){
+	for ( int i = 0 ; i < maxN ; i++ )
+		dis[i] = i;
+}
+
+inline int find ( int a ){
+	return dis[a] == a ? a : dis[a] = find ( dis[a] );
+}
+
+inline void Union ( int a, int b ){
+	dis[find ( a )] = find ( b );
+}
+
+inline bool same ( int a, int b ){
+	return find ( a ) == find ( b );
+}
+
 inline void Kruskal ( void ){
-	disjionSet dis;
-	dis.Init();
-	sort ( ALL ( edge ) ); // sort by weigh
-	REPALL ( i, edge ){
-		if ( dis.same ( i.u, i.v ) ) // check if it on the same tree
+	sort ( edges.begin(), edges.end(), cmp );
+	for ( auto i: edges ){ // C++ 11寫法，不懂再來問
+		if ( same ( i.u, i.v ) )
 			continue;
-		dis.Union ( i.u, i.v );
+		Union ( i.u, i.v );
 		mst[i.u].pb ( pii ( i.v, i.w ) );
 		mst[i.v].pb ( pii ( i.u, i.w ) );
 	}
