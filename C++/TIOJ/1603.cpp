e9278a9aa13e140f8c2eaa5561f8a2e574751fa2
Miohitokiri <lltzpp@gmail.com>
2018-05-26 16:10:06 +0800

    rewrite TIOJ 1063

--- C++/TIOJ/1603.cpp
+++ C++/TIOJ/1603.cpp
@@ -1,67 +1,69 @@
-// segment tree
 #include<bits/stdc++.h>
 
 using namespace std;
 
-struct segmentTree{
-	long long max, min;
-} seg[100005 * 4];
-long long basic[100005];
+#define maxN 100005
+typedef long long LL;
+typedef pair < LL, LL > pii;
+#define F first
+#define S second
 
-inline void build ( long long l, long long r, long long n );
-inline long long query ( long long l, long long r, long long nowL, long long nowR, long long n );
-inline long long queryMax ( long long l, long long r, long long nowL, long long nowR, long long n );
-inline long long queryMin ( long long l, long long r, long long nowL, long long nowR, long long n );
+#define int LL
+
+pii seg[maxN << 2];
+int basic[maxN];
+
+inline void build ( int l, int r, int n ){
+	if ( l == r )
+		seg[n] = pii ( basic[l], basic[l] );
+	else{
+		int mid = ( l + r ) >> 1, leftSon = n << 1, rightSon = leftSon | 1;
+		build ( l, mid, leftSon );
+		build ( mid + 1, r, rightSon );
+
+		seg[n].F = max ( seg[leftSon].F, seg[rightSon].F );
+		seg[n].S = min ( seg[leftSon].S, seg[rightSon].S );
+	}
+}
+
+inline int queryMa ( int l, int r, int nowL, int nowR, int n ){
+	if ( l <= nowL && nowR <= r )
+		return seg[n].F;
+	int mid = ( nowL + nowR ) >> 1, leftSon = n << 1, rightSon = leftSon | 1;
+	if ( r <= mid )
+		return queryMa ( l, r, nowL, mid, leftSon );
+	if ( mid < l )
+		return queryMa ( l, r, mid + 1, nowR, rightSon );
+	return max ( queryMa ( l, mid, nowL, mid, leftSon ), queryMa ( mid + 1, r, mid + 1, nowR, rightSon ) );
+}
+
+inline int queryMi ( int l, int r, int nowL, int nowR, int n ){
+	if ( l <= nowL && nowR <= r )
+		return seg[n].S;
+	int mid = ( nowL + nowR ) >> 1, leftSon = n << 1, rightSon = leftSon | 1;
+	if ( r <= mid )
+		return queryMi ( l, r, nowL, mid, leftSon );
+	if ( mid < l )
+		return queryMi ( l, r, mid + 1, nowR, rightSon );
+	return min ( queryMi ( l, mid, nowL, mid, leftSon ), queryMi ( mid + 1, r, mid + 1, nowR, rightSon ) );
+}
+
+#undef int
 
 int main(){
 	ios::sync_with_stdio ( false );
 	cin.tie ( 0 );
+	// cout.tie ( 0 );
+	#define int LL
 
-	long long n, stop, l, r, q;
-	cin >> n >> q;
-	
-	for ( long long i = 1 ; i <= n ; i++ )
+	int n, m, in, l, r;
+	cin >> n >> m;
+	for ( int i = 1 ; i <= n ; i++ )
 		cin >> basic[i];
-
 	build ( 1, n, 1 );
 
-	while ( q-- ){
+	while ( m-- ){
 		cin >> l >> r;
-		cout << queryMax ( l, r, 1, n, 1 ) - queryMin ( l, r, 1, n, 1 ) << '\n';
+		cout << queryMa ( l, r, 1, n, 1 ) - queryMi ( l, r, 1, n, 1 ) << '\n';
 	}
-}
-
-inline void build ( long long l, long long r, long long n ){
-	if ( l == r )
-		seg[n].max = seg[n].min = basic[r];
-	else{
-		int leftSon = n << 1, rightSon = leftSon + 1, mid = ( l + r ) >> 1;
-		build ( l, mid, leftSon );
-		build ( mid + 1, r, rightSon );
-		seg[n].max = max ( seg[leftSon].max, seg[rightSon].max );
-		seg[n].min = min ( seg[leftSon].min, seg[rightSon].min );
-	}
-}
-
-inline long long queryMax ( long long l, long long r, long long nowL, long long nowR, long long n ){
-	if ( l <= nowL && nowR <= r )
-		return seg[n].max;
-	long long nowMid = ( nowL + nowR ) >> 1, leftSon = n << 1, rightSon = leftSon + 1;
-	if ( r <= nowMid )
-		return queryMax ( l, r, nowL, nowMid, leftSon );
-	if ( nowMid < l )
-		return queryMax ( l, r, nowMid + 1, nowR, rightSon );
-	return max ( queryMax ( l, nowMid, nowL, nowMid, leftSon ), queryMax ( nowMid + 1, r, nowMid + 1, nowR, rightSon ) );
-}
-
-inline long long queryMin ( long long l, long long r, long long nowL, long long nowR, long long n ){
-	// cout << l << ' ' << r << '\n';
-	if ( l <= nowL && nowR <= r )
-		return seg[n].min;
-	long long nowMid = ( nowL + nowR ) >> 1, leftSon = n << 1, rightSon = leftSon + 1;
-	if ( r <= nowMid )
-		return queryMin ( l, r, nowL, nowMid, leftSon );
-	if ( nowMid < l )
-		return queryMin ( l, r, nowMid + 1, nowR, rightSon );
-	return min ( queryMin ( l, nowMid, nowL, nowMid, leftSon ), queryMin ( nowMid + 1, r, nowMid + 1, nowR, rightSon ) );
 }
