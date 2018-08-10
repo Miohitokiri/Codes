058712349240bd539e7cf13274d123a93b354e97
Miohitokiri <lltzpp@gmail.com>
2018-08-10 23:55:10 +0800

    back up

--- C++/UVa/Q00459.cpp
+++ C++/UVa/Q00459.cpp
@@ -10,33 +10,12 @@ typedef long long LL;
 
 // define tools
 #define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
 #define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define SP(a,b) swap ( a, b )
 #define MEM(n,i) memset ( n, i, sizeof n )
 
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
-#define pb push_back
-#define ep emplace_back
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-
-// define set
 typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) n.find ( Index ) != n.end()
+#define CLR(n) n.clear()
 
-// IO
 #define GL(n) getline ( cin, n )
 
 si edge[30];
@@ -75,7 +54,8 @@ int main(){
 			edge[v].insert ( u );
 		}
 
-		REPP ( i, 0, n + 1 )
+		n++;
+		REPP ( i, 0, n )
 			if ( !go[i] ){
 				ans++;
 				dfs ( i );
