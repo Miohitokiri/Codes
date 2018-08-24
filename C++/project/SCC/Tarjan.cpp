f4a4a18384cff7db3b9fd8be1c6a712f020af408
Miohitokiri <lltzpp@gmail.com>
2018-08-24 11:06:45 +0800

    Tarjan's SCC Algorithm update

--- C++/project/SCC/Tarjan.cpp
+++ C++/project/SCC/Tarjan.cpp
@@ -5,26 +5,37 @@ using namespace std;
 #define maxN 10005
 
 vector < int > edges[maxN];
-int L[maxN], D[maxN], id;
+int L[maxN], D[maxN], id, scc[maxN];
 stack < int > st;
+bool inSt[maxN];
 
-inline void dfs ( int n, int p, int dep ){
+inline void init ( void ){
+	memsest ( L, 0, sizeof L );
+	memset ( D, 0, sizeof D );
+	memset ( scc, -1, sizeof scc );
+	memset ( inSt, 0, sizeof inSt );
+	id = 0;
+	for ( auto &i: edges )
+		i.clear();
+	st.clear();
+}
+
+inline void dfs ( int n, int dep ){
 	D[n] = L[n] = dep++;
 	st.push ( n );
 	for ( auto i: edges[n] ){
-		if ( i == p )
-			continue;
 		if ( !D[i] ){
-			dfs ( i, p, dep );
+			dfs ( i, dep );
 			L[n] = min ( L[n], L[i] );
 		}
-		L[n] = min ( L[n], D[i] );
+		else if ( inSt[i] )
+			L[n] = min ( L[n], D[i] );
 	}
 
 	if ( D[n] == L[n] ){
 		int x;
 		do{
-			x = st.top();
+			inSt[x = st.top()] = false;
 			st.pop();
 			scc[x] = id;
 		} while ( x != n );
