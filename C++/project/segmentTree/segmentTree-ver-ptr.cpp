11ddd15ad57dfaac99c0b34cf0da871d2750dd20
Miohitokiri <lltzpp@gmail.com>
2018-08-25 09:52:09 +0800

    Segment Tree ver. ptr update

--- C++/project/segmentTree/segmentTree-ver-ptr.cpp
+++ C++/project/segmentTree/segmentTree-ver-ptr.cpp
@@ -1,71 +1,7 @@
 // by. MiohitoKiri5474
 #include<bits/stdc++.h>
 
-#pragma GCC optimize ( "O3" )
-#pragma loop_opt ( on )
-
 using namespace std;
-
-typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define SP(a,b) swap ( a, b )
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
-#define F first
-#define S second
-#define mp make_pair
-
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
-#define pb push_back
-#define ep emplace_back
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) n.find ( Index ) != n.end()
-
-// greph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
-#define GL(n) getline ( cin, n )
-
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
-
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
-#define INF 0x3f3f3f3f
-#define maxN 100005
-
-// ready~ go!
-// let's coding and have fun!
-// I can solve this problem!
 
 struct node{
 	int data;
@@ -80,9 +16,7 @@ struct node{
 	}
 } *root;
 
-int value, Index;
-
-inline node *build ( int l, int r ){
+node *build ( int l, int r ){
 	node *o = new node ( 0 );
 	if ( l == r )
 		return o;
@@ -92,21 +26,21 @@ inline node *build ( int l, int r ){
 	return o;
 }
 
-inline void insert ( int l, int r, node *&o ){
+void insert ( int l, int r, int index, int value, node *&o ){
 	if ( l == r )
 		o -> data = value;
 	else{
 		int mid = ( l + r ) >> 1;
-		if ( Index <= mid )
-			insert ( l, mid, o -> l );
+		if ( index <= mid )
+			insert ( l, mid, index, value, o -> l );
 		else
-			insert ( mid + 1, r, o -> r );
+			insert ( mid + 1, r, index, value, o -> r );
 
 		o -> up();
 	}
 }
 
-inline int query ( int l, int r, int nowL, int nowR, node *o ){
+int query ( int l, int r, int nowL, int nowR, node *o ){
 	if ( !o )
 		return 0;
 	if ( l <= nowL && nowR <= r )
@@ -118,41 +52,4 @@ inline int query ( int l, int r, int nowL, int nowR, node *o ){
 		return query ( l, r, nowL, nowMid, o -> l );
 	else
 		return max ( query ( l, nowMid, nowL, nowMid, o -> l ), query ( nowMid + 1, r, nowMid + 1, nowR, o -> r ) );
-}
-
-inline void dfs ( node *o ){ // del
-	cout << o -> data << ' ';
-	if ( o -> l )
-		dfs ( o -> l );
-	if ( o -> r )
-		dfs ( o -> r );
-}
-
-int main(){
-	ios::sync_with_stdio ( false );
-	cin.tie ( 0 );
-	cout.tie ( 0 );
-
-	int n, q, l, r, type;
-	cin >> n >> q;
-	n--;
-	root = build ( 0, n );
-	REPP ( i, 0, n + 1 ){
-		cin >> value;
-		insert ( 0, n, root );
-		Index++;
-	}
-
-	// del
-	dfs ( root );
-	cout << '\n';
-	// del
-
-	while ( q-- ){
-		cin >> type >> Index >> value;
-		if ( type == 1 )
-			insert ( 0, n, root );
-		else
-			cout << query ( Index, value, 0, n, root ) << '\n';
-	}
 }
