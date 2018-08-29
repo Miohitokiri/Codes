ddd78f3663319335824e0b5f25976a6bb440d4ec
Miohitokiri <lltzpp@gmail.com>
2018-08-29 11:37:18 +0800

    back up

--- C++/CodeForces/Education/R-037/pF.cpp
+++ C++/CodeForces/Education/R-037/pF.cpp
@@ -1,76 +1,14 @@
 // by. MiohitoKiri5474
 #include<bits/stdc++.h>
 
-#pragma GCC optimize ( "O3" )
-#pragma loop_opt ( on )
-
 using namespace std;
 
 typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
-#define F first
-#define S second
-#define mp make_pair
-
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
-#define pb push_back
-#define ep emplace_back
-#define REV reverse
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-#define GETDATA(data,n) RSZ ( data, n ); REPALL ( i, data ) cin >> i
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) n.find ( Index ) != n.end()
-
-// graph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
-#define GL(n) getline ( cin, n )
-
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
-
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
-#define INF 0x3f3f3f3f
 #define maxN 300005
-
-// ready~ go!
-// let's coding and have fun!
-// I can solve this problem!
 
 LL sum[maxN << 2], dp[1000005];
 bool used[maxN << 2];
-vi prime;
+vector < int > prime;
 bitset < 1005 > lib;
 
 inline int D ( int n ){
@@ -78,7 +16,8 @@ inline int D ( int n ){
 		return dp[n];
 	double www = sqrt ( n );
 	int ma = www, res = 0, maa = ma + 1;
-	REPP ( i, 1, maa ) n % i ? res : res++;
+	for ( int i = 1 ; i < maa ; i++ )
+		n % i ? res : res++;
 
 	return dp[n] = res * 2 - ( www == ma ? 1 : 0 );
 }
@@ -138,15 +77,15 @@ int main(){
 	cout.tie ( 0 );
 
 	lib[0] = lib[1] = true;
-	REPP ( i, 2, 1005 ){
+	for ( int i = 2 ; i < 1005 ; i++ ){
 		if ( !lib[i] ){
-			prime.pb ( i );
+			prime.push_back ( i );
 			for ( int j = i << 1 ; j < 1005 ; j += i )
 				lib[j] = true;
 		}
 	}
 
-	MEM ( dp, -1 );
+	memset ( dp, -1, sizeof dp );
 	int n, m, type, l, r, stop;
 	cin >> n >> m;
 	build ( 1, n, 1 );
