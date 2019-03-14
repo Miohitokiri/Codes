b126d8765f285a613389cd00f483647f5dba9791
MiohitoKiri <lltzpp@gmail.com>
2019-03-14 23:14:14 +0800

    back up

--- Golang/test.go
+++ Golang/test.go
@@ -1,16 +1,64 @@
 package main
+
 import (
+	// "io/ioutil"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
+
 func main(){
-	// var s, swp string
-	lib := "1234567"
-	for i := 1 ; i < len ( lib ) ; i++ {
-		fmt.Print ( lib[len ( lib ) - i] )
-		fmt.Print ( " " )
+	n := read()
+	k := read()
+	var (
+		in int
+		cnt[105] int
+	)
+	for i := 1 ; i < n ; i++ {
+		in = read()
+		cnt[in / k]++
+	}
+	cnt[0] /= 2
+	ans := cnt[0] * 2
+	if k % 2 == 0 {
+		cnt[k / 2] /= 2
+		ans += cnt[k / 2] * 2
 	}
 
-	fmt.Println()
-	// fmt.Println ( lib )
-	fmt.Println ( len ( lib ) )
+	for i := 1 ; i < k / 2 ; i++ {
+		ans += min ( cnt[i], cnt[k - 1] )
+	}
+
+	fmt.Println ( ans )
+}
+
+func read() int {
+	input := bufio.NewScanner ( os.Stdin )
+	input.Scan()
+	num, _ := strconv.Atoi ( input.Text() )
+	return num
+}
+
+func min ( a int, b int ) int {
+	if a > b{
+		tmp := a
+		a = b
+		b = tmp
+	}
+
+	return a
+}
+
+func gcd ( a int, b int ) int {
+	for a % b != 0 && b % a != 0 {
+		if a > b {
+			a %= b
+		}
+		else {
+			b %= a
+		}
+	}
+
+	return min ( a, b )
 }
