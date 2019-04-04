c65830fb4faa172316e13e0b6773cef0792a326b
MiohitoKiri <lltzpp@gmail.com>
2019-04-04 22:05:30 +0800

    remove some useless files

--- Golang/test.go
+++ Golang/test.go
@@ -40,9 +40,56 @@ func max ( a int, b int ) int {
 	return a
 }
 
+func _gcd ( a int, b int ) int {
+	for a % b != 0 && b % a != 0 {
+		if a > b {
+			a %= b
+		} else {
+			b %= a
+		}
+	}
+
+	return min ( a, b )
+}
+
 func main(){
-	n = make ( chan int )
+	n := make ( chan int )
 	n <- 12
 	data := <- n
 	fmt.Println ( data )
+}
+
+var seg[105 * 4] int
+
+func update ( l int, r int, index int, value int, n int ) {
+	if l == r {
+		seg[n] = value
+		return
+	}
+	mid := ( l + r ) / 2
+	leftSon := n * 2
+	rightSon := leftSon + 1
+	if index <= mid {
+		update ( l, mid, index, value, leftSon )
+	} else {
+		update ( mid + 1, r, index, value, rightSon )
+	}
+
+	seg[n] = max ( seg[leftSon], seg[rightSon] )
+}
+
+func query ( l int, r int, nowL int, nowR int, n int ) {
+	if l <= nowL && nowR <= r {
+		return seh[n]
+	}
+	mid := ( nowL + nowR ) / 2
+	leftSon := n * 2
+	rightSon := leftSon + 1
+	if r <= mid {
+		return query ( l, r, nowL, mid, leftSon )
+	}
+	if mid < l {
+		return query ( l, r, mid + 1, nowR, rightSon )
+	}
+	return max ( query ( l, r, nowL, mid, leftSon ), query ( l, r, nowL, mid, leftSon ) )
 }
