7c84a647add74dadbc53fd5aa7486965ad949c86
MiohitoKiri <lltzpp@gmail.com>
2019-04-06 12:52:08 +0800

    fix bugs

--- test.cpp
+++ test.cpp
@@ -4,15 +4,6 @@ using namespace std;
 
 vector < string > data[15];
 
-inline int count ( string str ){
-	int res = 0;
-	for ( char c: str )
-		if ( ( 'a' <= c && c <= 'z' ) || ( 'A' <= c && c <= 'Z' ) )
-			res++;
-
-	return res;
-}
-
 int main(){
 	ios::sync_with_stdio ( false );
 	cin.tie ( 0 );
@@ -20,7 +11,7 @@ int main(){
 
 	string str;
 	while ( cin >> str ){
-		data[count ( str )].push_back ( str );
+		data[str.size()].push_back ( str );
 	}
 
 	for ( int i = 1 ; i <= 10 ; i++ ){
