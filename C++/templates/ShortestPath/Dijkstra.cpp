777e79df84c142652bef4cea73c9c8f8550a9d7a
MiohitoKiri <lltzpp@gmail.com>
2019-06-30 14:28:57 +0800

    Dijkstra template upgrade

--- C++/templates/ShortestPath/Dijkstra.cpp
+++ C++/templates/ShortestPath/Dijkstra.cpp
@@ -1,4 +1,5 @@
 // Dijkstra
+// by. MiohitoKiri5474
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -12,11 +13,11 @@ int dis[maxN];
 
 inline void dijkstra ( int start ){
 	memset ( dis, 0x3f3f3f, sizeof dis );
-	priority_queue < pii, vector < pii >, greater < pii > > pq;
+	priority_queue < pii, vector < pii >, greater < pii > > pq; // 用自訂用vector實作、以及更改排序順序
 	pq.push ( pii ( 0, start ) );
 	dis[start] = 0;
 	while ( !pq.empty() ){
-		int d = pq.top().first, node = pq.top().second; // 取距離最短並在pri-queue中的點
+		int d = pq.top().first, node = pq.top().second; // 取距離最短並在pq中的點
 		pq.pop();
 		if ( dis[node] < d ) // 如果目前的路徑長比丟入pq時的路徑長還要短，直接忽略不處理
 			continue;
