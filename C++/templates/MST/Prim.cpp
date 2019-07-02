83fe522371570c7b6520fa8be70210541b0dec1d
MiohitoKiri <lltzpp@gmail.com>
2019-07-02 10:14:36 +0800

    Prim upgrade

--- C++/templates/MST/Prim.cpp
+++ C++/templates/MST/Prim.cpp
@@ -1,91 +1,29 @@
+// Prim
 // by. MiohitoKiri5474
 #include<bits/stdc++.h>
 
-#pragma GCC optimize ( "O3" )
-#pragma loop_opt ( on )
-
 using namespace std;
 
-typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
+#define pb push_back
 #define F first
 #define S second
-#define mp make_pair
+typedef pait < int, int > pii;
+typedef pair < int, pii > pipii;
+const int maxN = 100005;
 
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
-#define pb push_back
-#define ep emplace_back
-#define REV reverse
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-#define GETDATA(data,n) RSZ ( data, n ); REPALL ( i, data ) cin >> i
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) ( n.find ( Index ) != n.end() )
-
-// graph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
-#define GL(n) getline ( cin, n )
-
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
-
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
-#define INF 0x3f3f3f3f
-#define NEG_INF 0x8f8f8f8f
-#define maxN 100005
-
-// あの日見渡した渚を　今も思い出すんだ
-// 砂の上に刻んだ言葉　君の後ろ姿
-// 寄り返す波が　足元をよぎり何かを攫う
-// 夕凪の中　日暮れだけが通り過ぎて行く
-
-// ready~ go!
-// let's go coding and have fun!
-// I can solve this problem!
-
-GRE ( pii, edges );
-GRE ( pii, mst );
-int dis[maxN], sz[maxN];
+vector < pii > edges[maxN], mst[maxN];
+int dis[maxN];
 bool pushed[maxN];
 
 inline void init ( void ){
-	REPP ( i, 0, maxN ) dis[i] = i;
+	for ( int i = 0 ; i < maxN ; i++ )
+		dis[i] = i;
 }
 
 int find ( int n ){
 	return dis[n] == n ? n : dis[n] = find ( dis[n] );
 }
+
 
 inline void Union ( int a, int b ){
 	a = find ( a ), b = find ( b );
@@ -97,16 +35,13 @@ inline bool same ( int a, int b ){
 	return find ( a ) == find ( b );
 }
 
-typedef pair < int, pii > pp;
-
 inline void Pirm ( int n ){
-	MinHeap < pp > pq;
-	REPALL ( i, edges[0] ){
-		pq.push ( pp ( i.S, pii ( 0, i.F ) ) );
-	}
+	priority_queue < pipii, vector < pipii >, greater < pipii > > pq;
+	for ( auto i: edges[0] )
+		pq.push ( pipii ( i.S, pii ( 0, i.F ) ) );
 	pushed[0] = true;
 	while ( sz[find ( 0 )] != n ){
-		pp top = pq.top();
+		pipii top = pq.top();
 		pq.pop();
 		while ( same ( top.S.F, top.S.F ) ){
 			top = pq.top();
@@ -114,19 +49,18 @@ inline void Pirm ( int n ){
 		}
 		int u = top.S.F, v = top.S.S;
 
-		UNIw ( u, v, top.F, mst );
+		mst[u].pb ( pii ( v, top.F ) );
+		mst[v].pb ( pii ( u, top.F ) );
 		Union ( u, v );
 
 		if ( !pushed[u] ){
-			REPALL ( i, edges[u] ){
-				pq.push ( pp ( i.S, pii ( i.F, u ) ) );
-			}
+			for ( auto i: edges[u] )
+				pq.push ( pipii ( i.S, pii ( i.F, u ) ) );
 			pushed[u] = true;
 		}
 		if ( !pushed[v] ){
-			REPALL ( i, edges[v] ){
-				pq.push ( pp ( i.S, pii ( i.F, v ) ) );
-			}
+			for ( auto i: edges[v] )
+				pq.push ( pipii ( i.S, pii ( i.F, v ) ) );
 			pushed[v] = true;
 		}
 	}
