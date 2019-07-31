0b5eb515f48ed593a5cf3f7d348a723e1ce0af3a
MiohitoKiri <lltzpp@gmail.com>
2019-07-31 14:24:43 +0800

    TOJ 010 remove default code

--- C++/TOJ/010.cpp
+++ C++/TOJ/010.cpp
@@ -1,90 +1,31 @@
 // by. MiohitoKiri5474
 #include<bits/stdc++.h>
 
-#pragma GCC optimize ( "O3" )
-#pragma loop_opt ( on )
-
 using namespace std;
 
 typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
-#define F first
-#define S second
-#define mp make_pair
-
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
 #define pb push_back
-#define ep emplace_back
-#define REV reverse
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-#define GETDATA(data,n) RSZ ( data, n ); REPALL ( i, data ) cin >> i
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) ( n.find ( Index ) != n.end() )
-
-// graph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
-#define GL(n) getline ( cin, n )
-
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
-
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
 #define INF 0x3f3f3f3f
 #define NEG_INF 0x8f8f8f8f
 #define maxN 200010
 
-// ready~ go!
-// let's coding and have fun!
-// I can solve this problem!
-
-GRE ( int, edges );
+vector < int > edges[maxN];
 int where[maxN], value[maxN], link[maxN], pa[maxN], D[maxN], cnt, seg[maxN << 2], siz[maxN], maxSon[maxN];
 
 inline void init ( void ){
-	REPALL ( i, edges ) CLR ( i );
-	MEM ( where, -1 );
-	MEM ( value, NEG_INF );
-	MEM ( D, 0 );
-	MEM ( maxSon, -1 );
+	for ( auto &i: edges )
+		i.clear();
+	memset ( where, -1, sizeof where );
+	memset ( value, NEG_INF, sizeof value );
+	memset ( D, 0, sizeof D );
+	memset ( maxSon, -1, sizeof maxSon );
 	cnt = 0;
 }
 
 inline void findMaxSon ( int u ){
 	siz[u] = 1;
 	maxSon[u] = -1;
-	REPALL ( i, edges[u] ){
+	for ( auto i: edges[u] ){
 		if ( i == pa[u] )
 			continue;
 		pa[i] = u;
@@ -102,7 +43,7 @@ inline void dfs ( int n, int p ){
 	if ( maxSon[n] == -1 )
 		return;
 	dfs ( maxSon[n], p );
-	REPALL ( i, edges[n] ){
+	for ( auto i: edges[n] ){
 		if ( i == maxSon[n] || i == pa[n] )
 			continue;
 		dfs ( i, i );
@@ -147,18 +88,19 @@ int main(){
 		N = n << 1;
 		N--;
 		init();
-		REPP ( i, 1, n ){
+		for ( int i = 1 ; i < n ; i++ ){
 			cin >> u >> v >> value[n + i];
-			UNI ( u, n + i, edges );
-			UNI ( n + i, v, edges );
+			edges[u].pb ( n + i );
+			edges[n + i].pb ( u );
+			edges[n + i].pb ( v );
+			edges[v].pb ( n + i );
 		}
 
 		pa[1] = 1;
 		findMaxSon ( 1 );
 		dfs ( 1, 1 );
-		REPP ( i, 1, ( n << 1 ) ){
+		for ( int i = 1 ; i < ( n << 1 ) ; i++ )
 			update ( 1, N, where[i], value[i], 1 );
-		}
 		while ( cin >> type ){
 			if ( type == "DONE" )
 				break;
