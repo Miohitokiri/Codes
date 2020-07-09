e8b36c3c5de65d857488ad6dc7f421c23e0c4f4d
MiohitoKiri5474 <lltzpp@gmail.com>
2020-07-09 21:26:35 +0800

    backup 2020/07/09

---
+++ Golang/segmentTreePtr.go
@@ -0,0 +1,17 @@
+// by. MiohitoKiri5474
+package main
+
+import . "fmt"
+
+type node struct {
+	l, r *node
+	value, sz int
+}
+
+func update ( l int, r int, value int, index int, o *node ){
+	if l == r {
+	}
+}
+
+func main() {
+}
